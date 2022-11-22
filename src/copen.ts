c5d10bd11a5f6b48a351fde10c832cd23d9e8f61
Alex-Programer <alex.lichangnan@gmail.com>
2022-11-22 15:57:17 +0800

    fix

--- src/copen.ts
+++ src/copen.ts
@@ -1,13 +1,11 @@
 #!/usr/bin/env node --experimental-specifier-resolution=node
 
-import path from 'path'
 import { exec } from "./index";
 import { resolveRepoUrl } from './utils';
 
 const [, , repo] = process.argv;
 
 exec(["git", ["clone", repo]]).on('close', () => {
-  const [url] = path.basename(repo).split('.')
-  const fold = resolveRepoUrl(url);
+  const fold = resolveRepoUrl(repo);
   exec(["code", [fold]]);
 });
