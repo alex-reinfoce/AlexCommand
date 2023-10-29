576316ce71b5e2e2d9e3d18e33ac9ae256f36a62
Alex-Programer <alex.lichangnan@gmail.com>
2023-10-29 19:19:36 +0800

    feat: support sb

---
+++ src/cli/sb.ts
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import { exec } from "../index";
+
+exec(["nr", ["storybook"]]);
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ registerCommands([
   { name: "i", filename: "install.js" },
   { name: "s", filename: "start.js" },
   { name: "b", filename: "build.js" },
+  { name: "sb", filename: "sb.js" },
   { name: "e", filename: "e2e.js" },
   { name: "t", filename: "test.js" },
   { name: "copen", filename: "copen.js" },
