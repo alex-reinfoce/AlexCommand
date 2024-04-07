d71a2f446c4e6803822ab205200b218744940389
Alex-Programer <alex.lichangnan@gmail.com>
2024-04-07 14:54:41 +0800

    fix

--- src/cli/branch.ts
+++ src/cli/branch.ts
@@ -4,6 +4,6 @@ import { exec } from "..";
 exec([
   "git",
   [
-    'git for-each-ref --sort=-committerdate refs/heads/ --format="[%(committerdate:iso8601)] %(refname:short)"',
+    'for-each-ref --sort=-committerdate refs/heads/ --format="[%(committerdate:iso8601)] %(refname:short)"',
   ],
 ]);
